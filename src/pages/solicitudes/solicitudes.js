89dd57289841a2fbeea73a2148e1122c390cbc93
Anyi Bustos <anyi.bustos@mercadolibre.com.co>
2023-05-31 21:34:08 -0500

    upload and show a img

--- src/pages/solicitudes/solicitudes.js
+++ src/pages/solicitudes/solicitudes.js
@@ -1,10 +1,26 @@
 import Menu from '../../components/menu/menu';
+import CrearSolicitud from './crear-solicitud';
+import VerSolicitudes from './ver-solicitudes';
 
 
 function Solicitudes() {
     return (
-        <div>
+        <div class="container">
             Modulo Solicitudes
+            <nav>
+                <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Crear Solicitud</button>
+                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Ver Solicitudes</button>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+                    <CrearSolicitud />
+                </div>
+                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+                    <VerSolicitudes />
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/pages/solicitudes/ver-solicitudes.js
@@ -0,0 +1,12 @@
+
+
+function VerSolicitudes() {
+    return (
+
+        <div class="container">
+            Modulo ver solicitudes
+
+        </div>
+    );
+}
+export default VerSolicitudes;
