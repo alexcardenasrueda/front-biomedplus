758fbd0fe9722c4b102fdb2bc967fbf18aa62c55
Anyi Bustos <anyi.bustos@mercadolibre.com.co>
2023-05-31 21:53:53 -0500

    tabla ver solicitudes

--- src/pages/solicitudes/ver-solicitudes.js
+++ src/pages/solicitudes/ver-solicitudes.js
@@ -1,11 +1,50 @@
-
+import './ver-solicitudes.css';
 
 function VerSolicitudes() {
     return (
 
         <div class="container">
             Modulo ver solicitudes
-
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Estado solicitud</th>
+                        <th scope="col">Descripción solicitud</th>
+                        <th scope="col">Dias abierto</th>
+                        <th scope="col">Solicitante</th>
+                        <th scope="col">ID Solicitud</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <button type="button" class="btn btn-success btn-sm">Abierta</button>
+                        </td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-sm">En ejecución</button>
+                        </td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button type="button" class="btn btn-secondary btn-sm">Cerrada</button>
+                        </td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                        <td>Test</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
